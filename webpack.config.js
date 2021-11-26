ce9b1116306ba50ce2a03a7f577ced1e20ef9d3c
Tomas Ruiz <tomasruizr@gmail.com>
2021-11-26 12:51:52 -0300

    [update] dependencies and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 var path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   mode: 'none',
   entry: './build/index.js',
   output: {
-    filename: 'crocks.js',
+    filename: 'crocks.min.js',
     path: path.resolve(__dirname, 'build', 'dist'),
     library: 'crocks',
     libraryTarget: 'umd'
-  }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 }
