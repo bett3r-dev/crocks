18ff24ac2a63a857c74fb670a4d89f89eba341aa
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-12 10:07:25 -0300

    [update] tryCatch js-comment

--- src/Result/tryCatch.d.ts
+++ src/Result/tryCatch.d.ts
@@ -2,7 +2,7 @@ import { VariadicFunction } from '../interfaces/VariadicFunction';
 import Result from '.';
 
 /**
- * tryCatch :: ((*) -> b) -> (*) -> Result e b
+ * tryCatch :: ((args) -> b) -> (args) -> Result e b
  */
 declare function tryCatch<T>(fn: VariadicFunction<T>): VariadicFunction<Result<T>>;
 
