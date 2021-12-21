26cad41bf165007d180f2f34352792876d332c51
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-21 10:13:39 -0300

    [add] List

--- src/List/List.d.ts
+++ src/List/List.d.ts
@@ -1,3 +1,15 @@
-declare type List = any;
+import { Applicative, Functor, Monad, UnaryFunction } from "../interfaces";
 
+declare function List<T>(value: T): List<T>;
+
+declare class List<Right = any> implements Functor<Right>, Monad<Right>, Applicative<Right> {
+    _type: Right;
+    map<RETURN=Right, R= RETURN>(fn: UnaryFunction<RETURN, R>): List<RETURN>;
+    chain<RETURN=Right, R=Right>(fn: UnaryFunction<List<RETURN>, R>): List<RETURN>;
+    ap<R=Right>(val: List): List<R>;
+    traverse<R=Right, POINT = any>(point: POINT, transformFn: (value: R) => R): POINT
+    valueOf(): Right;
+    type():string;
+    static of<R>(val?: R): List<R>;
+}
 export default List;
