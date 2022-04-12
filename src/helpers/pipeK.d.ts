2269d3c2f8905a6460e015d8c7f3e83177b55fed
Tomas Ruiz <tomasruizr@gmail.com>
2022-04-12 02:11:31 -0300

    [update] pipeK types

--- src/helpers/pipeK.d.ts
+++ src/helpers/pipeK.d.ts
@@ -3,6 +3,6 @@ import {UnaryFunction} from '../interfaces/index'
  * pipeK :: Chain m => ((a -> m b), ..., (y -> m z)) -> a -> m z
  */
 // declare function pipeK<A extends any[], Z>(f: (...args: A) => unknown, g: ReadonlyArray<(y: unknown) => Z>): Z;
-declare function pipeK<Z>(head: UnaryFunction, ...tail: UnaryFunction[]): Z;
+declare function pipeK<Z, V=any>(head: UnaryFunction, ...tail: UnaryFunction[]): (initialVal?: V) => Z;
 
 export default pipeK;
